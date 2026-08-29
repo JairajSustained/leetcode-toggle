edcdb6f64403978c36908b1b4c1ce385aa00d8f2
JairajSustained <158572553+JairajSustained@users.noreply.github.com>
2026-08-29 17:00:47 +0530

    Fix CI: secrets context not allowed in if (no-op step instead); version 1.0.1

--- build.sh
+++ build.sh
@@ -81,7 +81,7 @@ cat > "$APP/Contents/Info.plist" <<PLIST
   <key>CFBundleIdentifier</key>      <string>$BUNDLE_ID</string>
   <key>CFBundleInfoDictionaryVersion</key> <string>6.0</string>
   <key>CFBundlePackageType</key>     <string>APPL</string>
-  <key>CFBundleShortVersionString</key> <string>1.0.0</string>
+  <key>CFBundleShortVersionString</key> <string>1.0.1</string>
   <key>CFBundleVersion</key>         <string>1</string>
   <key>LSMinimumSystemVersion</key>  <string>14.0</string>
   <key>LSUIElement</key>             <true/>
